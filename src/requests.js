d4f20193fda0ed68c6a49965bb64cc8a46e0abb5
deepaklogin <deepakparsabad2@gmail.com>
2020-10-31 17:52:58 +0530

    netflix-clone
    netflix-clone

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY="8b1bfc5d1a8e296a1173b9cf8569c869";
+
+const requests={
+    fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&width_networks=213`,
+    fetchTopRated:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovie:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
